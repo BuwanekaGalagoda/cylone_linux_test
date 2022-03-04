d936fc61d368eafcfadb08aae122d53120a3f119
BuwanekaGalagoda <Buwanekagalagoda@gmail.com>
2022-03-04 20:18:25 +0530

    terview2

--- resources/views/territory/index.blade.php
+++ resources/views/territory/index.blade.php
@@ -1,43 +1,23 @@
-@extends('territory.layout')
 <!DOCTYPE html>
 <html>
 <head>
    <title>Add Territory</title>
 </head>
 <body>
-    @section('content')
-    <div class="pull-left">
-        <h2>Add Zone</h2>
-    </div>
-    <div class="row">
-        <div class="col-lg-12 margine-tb">
-            <div class="pull-right">
-                <a class="btn btn-success" href="{{route('territory.create')}}">Add Zone</a>
-            </div>
-        </div>
     
-    </div>
-
-    @if($message=Session::get('succss'))
-
-    <div class="alert alert-success">
-        <p>{{$message}}</p>
-    </div>
-    @endif
-    
-   <!-- Department Dropdown -->
+   <!-- zone Dropdown -->
    Zone : <select id='zone' name='zone'>
       <option value='0'>-- Select Zone --</option>
 
-      <!-- Read Departments -->
-      @foreach($zone['data'] as $zone)
+      <!-- Read zones -->
+      {{-- @foreach($zone['data'] as $zone)
         <option value='{{ $zone->id }}'>{{ $zone->sdes }}</option>
-      @endforeach
+      @endforeach --}}
 
    </select>
 
    <br><br>
-   <!-- Department Employees Dropdown -->
+   <!-- regions Dropdown -->
    Region : <select id='region' name='region'>
      <option value='0'>-- Select Region --</option>
    </select>
@@ -47,10 +27,10 @@
    <script type='text/javascript'>
    $(document).ready(function(){
 
-      // Department Change
-      $('#szone').change(function(){
+      // zone Change
+      $('#zone').change(function(){
 
-         // Department id
+         // zone id
          var id = $(this).val();
 
          // Empty the dropdown
